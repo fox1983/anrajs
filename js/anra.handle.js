10ea7086c50600acc6a6c3c9039073c27491a625
anrainie <anrainie@163.com>
2016-08-30 17:23:43 +0800

    优化dragTracker
    主要是缩减代码提高可读性

--- js/anra.handle.js
+++ js/anra.handle.js
@@ -4,26 +4,26 @@
 var Control = anra.svg.Control;
 anra.Handle = Control.extend({
     //const
-    defaultWidth: 4,
-    defaultHeight: 4,
-    offset: 2,
+    defaultWidth:4,
+    defaultHeight:4,
+    offset:2,
     //data
-    editPart: null,
-    direction: null,
-    constructor: function (editPart, direction) {
+    editPart:null,
+    direction:null,
+    constructor:function (editPart, direction) {
         this._Control(); //ï¿½ï¿½ï¿½Ã¸ï¿½ï¿½Ä¹ï¿½ï¿½ìº¯ï¿½ï¿½
         this.editPart = editPart;
         this.direction = direction;
         var model = editPart.model;
         if (model != null) {
             var handle = this;
-            // µ±figure±»ÍÏ¶¯µÄÊ±ºò,Ë¢ÐÂhandles
+            // ï¿½ï¿½figureï¿½ï¿½ï¿½Ï¶ï¿½ï¿½ï¿½Ê±ï¿½ï¿½,Ë¢ï¿½ï¿½handles
             editPart.figure.addListener(anra.EVENT.MouseDrag, function () {
                 handle.setLocator(editPart.model.getBounds());
             });
             this.setLocator(model.getBounds());
             this.setStyle({
-                'fill': '#000000'
+                'stroke':'#000000'
             });
             var dt = this.getResizeTracker(direction);
             this.addListener(anra.EVENT.MouseDown, function (e) {
@@ -42,7 +42,7 @@ anra.Handle = Control.extend({
                     dt.dragEnd(e, editPart);
             });
             this.addListener(anra.EVENT.MouseDrag, function (e) {
-                console.log("mouse Drag" + e.x + e.y);
+                console.log("mouse Drag" + e.x + e.y, direction, dt);
                 if (dt != null)
                     dt.mouseDrag(e, editPart);
             });
@@ -55,147 +55,98 @@ anra.Handle = Control.extend({
 
     },
 
-    setLocator: function (bounds) {
+    setLocator:function (bounds) {
         var width = bounds[2];
         var height = bounds[3];
         var x = bounds[0] - this.offset;
         var y = bounds[1] - this.offset;
-        var cursorStyle = 'default';
-        switch (this.direction) {
-            //ï¿½ï¿½ï¿½ï¿½ï¿½
-            case anra.Handle.NORTH_WEST:
-                cursorStyle = 'nw-resize';
-                break;
-            case anra.Handle.NORTH:
-                x += width / 2;
-                cursorStyle = 'n-resize';
-                break;
-            case anra.Handle.NORTH_EAST:
-                x += width;
-                cursorStyle = 'ne-resize';
-                break;
-            //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
-            case anra.Handle.WEST:
-                y += height / 2;
-                cursorStyle = 'w-resize';
-                break;
-            case anra.Handle.EAST:
-                x += width;
-                y += height / 2;
-                cursorStyle = 'e-resize';
-                break;
-            //ï¿½ï¿½ï¿½ï¿½ï¿½
-            case anra.Handle.SOUTH_WEST:
-                y += height;
-                cursorStyle = 'sw-resize';
-                break;
-            case anra.Handle.SOUTH:
-                x += width / 2;
-                y += height;
-                cursorStyle = 's-resize';
-                break;
-            case anra.Handle.SOUTH_EAST:
-                x += width;
-                y += height;
-                cursorStyle = 'se-resize';
-                break;
-            default :
+        var cursorStyle;
 
+        cursorStyle = this.direction + '-resize';
+        if (this.direction.search('e') != -1) {
+            x += width;
+        }
+        console.log(this.direction, this.direction.search('(we){1}'));
+        if (this.direction.search('(w|e)') == -1) {
+            x += width / 2;
+        }
+        if (this.direction.search('s') != -1) {
+            y += height;
+        }
+        if (this.direction.search('(n|s)') == -1) {
+            y += height / 2;
         }
         this.setBounds({
-            x: x,
-            y: y,
-            width: this.defaultWidth,
-            height: this.defaultHeight
+            x:x,
+            y:y,
+            width:this.defaultWidth,
+            height:this.defaultHeight
         });
         this.setStyle({
-            'cursor': cursorStyle
+            'cursor':cursorStyle
         });
     },
-    refreshLocation: function () {
+    refreshLocation:function () {
         this.setLocator(this.editPart.model.getBounds());
     },
-    getResizeTracker: function (direction) {
-        var tracker = null;
-        switch (direction) {
-            case anra.Handle.NORTH_WEST:
-                tracker = anra.gef.NorthWestTracker();
-                break;
-            case anra.Handle.NORTH:
-                tracker = anra.gef.NorthTracker();
-                break;
-            case anra.Handle.NORTH_EAST:
-                tracker = anra.gef.NorthEastTracker();
-                break;
-
-            case anra.Handle.WEST:
-                tracker = anra.gef.WestTracker();
-                break;
-            case anra.Handle.EAST:
-                tracker = anra.gef.EastTracker();
-                break;
-
-            case anra.Handle.SOUTH_WEST:
-                tracker = anra.gef.SouthWestTracker();
-                break;
-            case anra.Handle.SOUTH:
-                tracker = anra.gef.SouthTracker();
-                break;
-            case anra.Handle.SOUTH_EAST:
-                tracker = anra.gef.SouthEastTracker();
-                break;
-            default :
-                tracker = anra.gef.getResizeTracker();
-        }
-        return tracker;
+    getResizeTracker:function (direction) {
+        return  anra.gef.ResizeTracker.getInstance(direction);
     }
 });
 //ï¿½ï¿½ï¿½å·½ï¿½ï¿½ï¿½ï¿½
-anra.Handle.NORTH = "north";
-anra.Handle.SOUTH = "south";
-anra.Handle.EAST = "east";
-anra.Handle.WEST = "west";
-anra.Handle.NORTH_EAST = "northeast";
-anra.Handle.NORTH_WEST = "northwest";
-anra.Handle.SOUTH_EAST = "southeast";
-anra.Handle.SOUTH_WEST = "southwest";
+anra.Handle.NORTH = "n";
+anra.Handle.SOUTH = "s";
+anra.Handle.EAST = "e";
+anra.Handle.WEST = "w";
+anra.Handle.NORTH_EAST = "ne";
+anra.Handle.NORTH_WEST = "nw";
+anra.Handle.SOUTH_EAST = "se";
+anra.Handle.SOUTH_WEST = "sw";
 
-anra.gef.getResizeTracker = Base.extend({
-    status: null,
-    xStart: 0,
-    yStart: 0,
-    oldConstraint: null,
-    mouseDown: function (me, editPart) {
+anra.gef.ResizeTracker = Base.extend({
+    status:null,
+    xStart:0,
+    yStart:0,
+    oldConstraint:null,
+    mouseDown:function (me, editPart) {
         this.status = me.type;
     },
-    dragStart: function (me, editPart) {
+    dragStart:function (me, editPart) {
         this.status = me.type;
         this.xStart = me.x;
         this.yStart = me.y;
         this.oldConstraint = {
-            x: editPart.model.getBounds()[0],
-            y: editPart.model.getBounds()[1],
-            width: editPart.model.getBounds()[2],
-            height: editPart.model.getBounds()[3]
+            x:editPart.model.getBounds()[0],
+            y:editPart.model.getBounds()[1],
+            width:editPart.model.getBounds()[2],
+            height:editPart.model.getBounds()[3]
         };
 
     },
-    dragEnd: function (me, editPart) {
+    dragEnd:function (me, editPart) {
         this.status = me.type;
         editPart.editor.execute(new anra.gef.RelocalCommand(editPart, this.oldConstraint, {
-            x: editPart.model.getBounds()[0],
-            y: editPart.model.getBounds()[1],
-            width: editPart.model.getBounds()[2],
-            height: editPart.model.getBounds()[3]
+            x:editPart.model.getBounds()[0],
+            y:editPart.model.getBounds()[1],
+            width:editPart.model.getBounds()[2],
+            height:editPart.model.getBounds()[3]
         }));
     },
-    mouseUp: function (me, editPart) {
+    mouseUp:function (me, editPart) {
         this.status = me.type;
     }
-})
-;
-anra.gef.NorthWestTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+});
+
+anra.gef.ResizeTracker.getInstance = function (direction) {
+    var tracker = trackPool.get(direction);
+    if (tracker == null)
+        return new anra.gef.ResizeTracker();
+    return tracker;
+}
+
+
+anra.gef.NorthWestTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[0] = this.oldConstraint.x + (me.x - this.xStart);
         editPart.model.getBounds()[1] = this.oldConstraint.y + (me.y - this.yStart);
@@ -204,16 +155,16 @@ anra.gef.NorthWestTracker = anra.gef.getResizeTracker.extend({
         editPart.refresh();
     }
 });
-anra.gef.NorthTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+anra.gef.NorthTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[1] = this.oldConstraint.y + (me.y - this.yStart);
         editPart.model.getBounds()[3] = this.oldConstraint.height - (me.y - this.yStart);
         editPart.refresh();
     }
 });
-anra.gef.NorthEastTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+anra.gef.NorthEastTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[1] = this.oldConstraint.y + (me.y - this.yStart);
         editPart.model.getBounds()[2] = this.oldConstraint.width + (me.x - this.xStart);
@@ -222,24 +173,24 @@ anra.gef.NorthEastTracker = anra.gef.getResizeTracker.extend({
     }
 });
 
-anra.gef.WestTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+anra.gef.WestTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[0] = this.oldConstraint.x + (me.x - this.xStart);
         editPart.model.getBounds()[2] = this.oldConstraint.width - (me.x - this.xStart);
         editPart.refresh();
     }
 });
-anra.gef.EastTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+anra.gef.EastTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[2] = this.oldConstraint.width + (me.x - this.xStart);
         editPart.refresh();
     }
 });
 
-anra.gef.SouthWestTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+anra.gef.SouthWestTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[0] = this.oldConstraint.x + (me.x - this.xStart);
         editPart.model.getBounds()[2] = this.oldConstraint.width - (me.x - this.xStart);
@@ -247,18 +198,29 @@ anra.gef.SouthWestTracker = anra.gef.getResizeTracker.extend({
         editPart.refresh();
     }
 });
-anra.gef.SouthTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+anra.gef.SouthTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[3] = this.oldConstraint.height + (me.y - this.yStart);
         editPart.refresh();
     }
 });
-anra.gef.SouthEastTracker = anra.gef.getResizeTracker.extend({
-    mouseDrag: function (me, editPart) {
+anra.gef.SouthEastTracker = anra.gef.ResizeTracker.extend({
+    mouseDrag:function (me, editPart) {
         this.status = me.type;
         editPart.model.getBounds()[2] = this.oldConstraint.width + (me.x - this.xStart);
         editPart.model.getBounds()[3] = this.oldConstraint.height + (me.y - this.yStart);
         editPart.refresh();
     }
 });
+
+
+var trackPool = new Map();
+trackPool.put(anra.Handle.NORTH, new anra.gef.NorthTracker());
+trackPool.put(anra.Handle.SOUTH, new anra.gef.SouthTracker());
+trackPool.put(anra.Handle.SOUTH_EAST, new anra.gef.SouthEastTracker());
+trackPool.put(anra.Handle.SOUTH_WEST, new anra.gef.SouthWestTracker());
+trackPool.put(anra.Handle.NORTH_WEST, new anra.gef.NorthWestTracker());
+trackPool.put(anra.Handle.NORTH_EAST, new anra.gef.NorthEastTracker());
+trackPool.put(anra.Handle.EAST, new anra.gef.EastTracker());
+trackPool.put(anra.Handle.WEST, new anra.gef.WestTracker());
